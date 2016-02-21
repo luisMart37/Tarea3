996e18ca42ebf4f232f529618492100b717a8636
Admin <Admin>
2016-02-21 12:22:23 +0100

    Hito 2. Botones en la primera actividad que lanzan logs al ser pulsados

--- Tarea3/app/src/main/java/com/upv/luissoyyo/tarea3/MainActivity.java
+++ Tarea3/app/src/main/java/com/upv/luissoyyo/tarea3/MainActivity.java
@@ -2,6 +2,8 @@ package com.upv.luissoyyo.tarea3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void pulsadoBoton1(View view){
+        Log.d("prueba", "Pulsado boton 1");
+    }//pulsadoBoton1
+    public void pulsadoBoton2(View view){
+        Log.d("prueba", "Pulsado boton 2");
+    }//pulsadoBoton2
 }
