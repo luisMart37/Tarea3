09712c33a74f1576070c3fe9a28a07847118d6fe
Admin <Admin>
2016-02-23 09:31:46 +0100

    Hito 5. La segunda actividad hace la suma y la muestra en un log.

--- Tarea3/app/src/main/java/com/upv/luissoyyo/tarea3/Activity2.java
+++ Tarea3/app/src/main/java/com/upv/luissoyyo/tarea3/Activity2.java
@@ -1,19 +1,43 @@
 package com.upv.luissoyyo.tarea3;
 
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
+    private TextView textSumando1, textSumando2;
+    private int sumando1, sumando2, suma;
+    private String stringSumando1, stringSumando2;
+
+    //POR QUÉ PRIVATE?
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d("prueba","entra en la funcion onCreate de Main2");
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        Log.d("prueba","Arranca Activity2");
+        super.onCreate(savedInstanceState);//constructor del padre
+        setContentView(R.layout.activity_main2);//relación de Activity2.java
+        //con su layout
+
     }
     public void pulsadoBotonSumar(View view){
-        Log.d("prueba","pulsado el boton SUMAR");
+        Log.d("prueba", "pulsado el boton SUMAR");
+        //vamos a leer del cuadro de texto
+        textSumando1=(EditText)findViewById(R.id.editTextSumando1);
+        //convertimos el texto del cuadro en una variable String: stringSumando1
+        stringSumando1=textSumando1.getText().toString();
+        Log.d("prueba",stringSumando1);
+        sumando1=Integer.parseInt(stringSumando1);
+        //repetimos las dos líneas anteriores para el cuadro 2
+        textSumando2=(EditText) findViewById(R.id.editTextSumando2);
+        stringSumando2=textSumando2.getText().toString();
+        sumando2=Integer.parseInt(stringSumando2);
+        //hacemos la suma
+        suma=sumando1+sumando2;
+        Log.d("prueba","El resultado de la suma es "+suma);
     }//pulsadoBotonSumar
+
 }
--- Tarea3/app/src/main/java/com/upv/luissoyyo/tarea3/MainActivity.java
+++ Tarea3/app/src/main/java/com/upv/luissoyyo/tarea3/MainActivity.java
@@ -10,13 +10,16 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);//constructor del padre
+        setContentView(R.layout.activity_main);//layout correspondiente
     }
     public void pulsadoBoton1(View view){
         Log.d("prueba", "Pulsado boton 1");
+        //creamos la intención de abrir la Activity2
         Intent intencion1 = new Intent(this, Activity2.class);
         //Log.d("prueba","pasa por encima del intent");
+        //arrancamos la intención de abrir Activity2, de la que esperamos
+        //un resultado. La reconoceremos con el índice 1
         super.startActivityForResult(intencion1,1);
     }//pulsadoBoton1
     public void pulsadoBoton2(View view){
